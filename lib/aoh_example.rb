512a01a7d67f721ac4b307cf6a57a87f427e5028
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-06 19:55:14 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,7 +9,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
-  assabled = [
+  result = [
     DON_G,
     JOELLE_VD,
     PAT_M,
@@ -21,6 +21,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  
 end
 
 def aoh_lookup(aoh, row, key)
